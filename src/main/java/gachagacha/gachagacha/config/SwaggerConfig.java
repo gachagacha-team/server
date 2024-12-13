a0f825c03cdd0928f21ae84dfde89ad6659c4102
olsohee <dlthgml0108@daum.net>
2024-12-13 16:53:47 +0900

    chore: 스웨거 설정

---
+++ src/main/java/gachagacha/gachagacha/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package gachagacha.gachagacha.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .version("v1")
+                .title("GachaGacha API");
+        return new OpenAPI()
+                .info(info);
+    }
+}
