60d219d15fea5f69282607792d3603b30d1bc8d6
olsohee <dlthgml0108@daum.net>
2025-02-13 14:12:53 +0900

    chore: 로그 필터 설정 추가

--- src/main/java/gachagacha/gachagacha/config/WebConfig.java
+++ src/main/java/gachagacha/gachagacha/config/WebConfig.java
@@ -1,7 +1,11 @@
 package gachagacha.gachagacha.config;
 
+import gachagacha.gachagacha.filter.LogFilter;
 import gachagacha.gachagacha.interceptor.AuthInterceptor;
+import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -16,9 +20,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
+        ;
     }
 
     @Override
@@ -28,7 +34,17 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/", "/swagger-ui/**", "/v3/api-docs/**", "/css/**", "/*.ico", "/error",
-                        "/login/oauth2/code/github", "/login/oauth2/code/kakao", "/join",
-                        "/image/**");
+                        "/login/oauth2/code/github", "/login/oauth2/code/kakao", "/join/**",
+                        "/image/**",
+                        "/auth");
+    }
+
+    @Bean
+    public FilterRegistrationBean<Filter> logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
     }
 }
