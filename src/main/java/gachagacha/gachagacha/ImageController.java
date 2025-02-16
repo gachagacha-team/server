ead87049c8cd8bdefed65f5f00b3464b6ef5206b
olsohee <dlthgml0108@daum.net>
2025-02-16 23:00:35 +0900

    fix: 프로필 이미지 파일 저장 위치 변경

--- src/main/java/gachagacha/gachagacha/ImageController.java
+++ src/main/java/gachagacha/gachagacha/ImageController.java
@@ -1,14 +1,23 @@
 package gachagacha.gachagacha;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.IOException;
+
 @RestController
 public class ImageController {
+
+    @Value("${file.profile}")
+    private String fileDir;
 
     @GetMapping(value = "/image/items/{imageName}", produces = MediaType.IMAGE_GIF_VALUE)
     public ResponseEntity getItemImage(@PathVariable String imageName) {
@@ -29,8 +38,10 @@ public class ImageController {
     }
 
     @GetMapping(value = "/image/profile/{imageName}", produces = MediaType.IMAGE_PNG_VALUE)
-    public ResponseEntity getProfileImage(@PathVariable String imageName) {
-        ClassPathResource resource = new ClassPathResource("/profile/" + imageName);
-        return ResponseEntity.ok().body(resource);
+    public ResponseEntity getProfileImage(@PathVariable String imageName) throws IOException {
+        File imageFile = new File(System.getProperty("user.dir") + fileDir + imageName);
+        Resource resource = new FileSystemResource(imageFile);
+        return ResponseEntity.ok()
+                .body(resource);
     }
 }
