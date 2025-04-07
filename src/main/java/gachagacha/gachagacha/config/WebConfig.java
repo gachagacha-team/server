cecdda1396d80d5dba757b9e0a5f452b03b70449
olsohee <dlthgml0108@daum.net>
2025-04-07 14:30:38 +0900

    fix: cors 설정

--- src/main/java/gachagacha/gachagacha/config/WebConfig.java
+++ src/main/java/gachagacha/gachagacha/config/WebConfig.java
@@ -20,11 +20,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("https://gacha-ruddy.vercel.app")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
-        ;
     }
 
     @Override
