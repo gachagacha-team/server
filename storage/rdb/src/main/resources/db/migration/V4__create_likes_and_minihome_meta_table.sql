f742ca02a4bdd69837cc991583085e6e8ab37e52
olsohee <dlthgml0108@daum.net>
2025-05-12 13:48:42 +0900

    chore: likes, minihome_meta DDL 파일 생성

---
+++ storage/rdb/src/main/resources/db/migration/V4__create_likes_and_minihome_meta_table.sql
@@ -0,0 +1,17 @@
+drop table if exists likes;
+drop table if exists minihome_meta;
+
+create table likes
+(
+    likes_id    bigint auto_increment primary key,
+    minihome_id bigint not null,
+    created_at  datetime(0) not null,
+    updated_at  datetime(0) not null
+);
+
+create table minihome_meta
+(
+    minihome_meta_id bigint auto_increment primary key,
+    minihome_id      bigint not null,
+    like_count       bigint not null
+);
